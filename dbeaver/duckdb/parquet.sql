f83c6ad162133a6c6bf71c5ff8592da176f26a8b
Cleverson Leite da Rocha <cleverson.rocha82@gmail.com>
2026-06-12 19:51:28 -0300

    feat: SQL utilizado no DBeaver para auditar os arquivos parquet gerados.

---
+++ dbeaver/duckdb/parquet.sql
@@ -0,0 +1,21 @@
+-- 1. Instala e carrega a extensão de rede/S3
+INSTALL httpfs;
+LOAD httpfs;
+
+-- 2. Configura as credenciais do seu MinIO local
+SET s3_endpoint='localhost:9000';  -- A porta da API do MinIO (geralmente 9000)
+SET s3_access_key_id='cleverson';
+SET s3_secret_access_key='cleverson';
+SET s3_use_ssl=false; -- Como é localhost, não usa HTTPS
+SET s3_region='us-east-1'; -- Valor padrão, apenas para preencher o requisito do protocolo
+SET s3_url_style='path'; -- MUITO IMPORTANTE para o MinIO local funcionar!
+
+SELECT * FROM read_parquet('s3://silver/cnaes/cnaes.parquet');
+SELECT * FROM read_parquet('s3://silver/motivos/motivos.parquet');
+SELECT * FROM read_parquet('s3://silver/municipios/municipios.parquet');
+SELECT * FROM read_parquet('s3://silver/naturezas/naturezas.parquet');
+SELECT * FROM read_parquet('s3://silver/paises/paises.parquet');
+SELECT * FROM read_parquet('s3://silver/qualificacoes/qualificacoes.parquet');
+SELECT * FROM read_parquet('s3://silver/socios/socios.parquet') LIMIT 1000;
+SELECT * FROM read_parquet('s3://silver/empresas/empresas.parquet') LIMIT 1000;
+SELECT * FROM read_parquet('s3://silver/estabelecimentos/estabelecimentos.parquet') LIMIT 1000;
