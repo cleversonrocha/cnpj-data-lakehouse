88931f713192092056d1f5be71253809fd6d3d9c
Cleverson Leite da Rocha <cleverson.rocha82@gmail.com>
2026-06-18 15:15:47 -0300

    fix: - Alterando conversão para "CAST", para portabilidade de código.

--- dbeaver/duckdb/parquet.sql
+++ dbeaver/duckdb/parquet.sql
@@ -1,6 +1,8 @@
 -- 1. Instala e carrega a extensão de rede/S3
 INSTALL httpfs;
 LOAD httpfs;
+INSTALL zipfs FROM community; 
+LOAD zipfs;
 
 -- 2. Configura as credenciais do seu MinIO local
 SET s3_endpoint='localhost:9000';  -- A porta da API do MinIO (geralmente 9000)
@@ -19,3 +21,18 @@ SELECT * FROM read_parquet('s3://silver/cleaned/qualificacoes.parquet');
 SELECT * FROM read_parquet('s3://silver/cleaned/socios.parquet') LIMIT 1000;
 SELECT * FROM read_parquet('s3://silver/cleaned/empresas.parquet') LIMIT 1000;
 SELECT * FROM read_parquet('s3://silver/cleaned/estabelecimentos.parquet') LIMIT 1000;
+
+SELECT count(*) FROM read_parquet('s3://silver/raw/empresas.parquet') em 
+JOIN read_parquet('s3://silver/cleaned/estabelecimentos.parquet') es ON es.cnpj_basico = em.cnpj_basico
+JOIN read_parquet('s3://silver/cleaned/municipios.parquet') mu ON mu.codigo = es.municipio
+JOIN read_parquet('s3://silver/cleaned/naturezas.parquet') n ON n.codigo = em.natureza_juridica
+JOIN read_parquet('s3://silver/cleaned/cnaes.parquet') cfp ON cfp.codigo = es.cnae_fiscal_principal
+LEFT JOIN read_parquet('s3://silver/cleaned/qualificacoes.parquet') q ON q.codigo = em.qualificacao_responsavel
+WHERE razao_social LIKE 'SILVIO LEITE ROCHA%';
+
+SELECT count(*) FROM read_csv_auto('zip://s3://bronze/raw/2026_06/Estabelecimentos*.zip',
+									sep=';', 
+                        			header=False,  
+                        			encoding='utf-8',
+                        			all_varchar=true,                    
+                        			ignore_errors=true);
--- dbt/models/silver_empresas.sql
+++ dbt/models/silver_empresas.sql
@@ -3,12 +3,14 @@
 ) }}
 
 SELECT    
-    column0::VARCHAR AS cnpj_basico,        
-    column1::VARCHAR AS razao_social,
-    column2::INTEGER AS natureza_juridica,
-    column3::INTEGER AS qualificacao_responsavel,
-    column5::INTEGER AS porte_empresa,
-    column6::VARCHAR AS ente_federativo_responsavel,         
+    CAST(column0 AS VARCHAR) AS cnpj_basico,        
+    CAST(column1 AS VARCHAR) AS razao_social,
+    CAST(column2 AS INTEGER) AS natureza_juridica,
+    CAST(column3 AS INTEGER) AS qualificacao_responsavel,
+    CAST(column5 AS INTEGER) AS porte_empresa,
+    CAST(column6 AS VARCHAR) AS ente_federativo_responsavel,         
     TRY_CAST(REPLACE(column4, ',', '.') AS DECIMAL(15, 2)) AS capital_social,        
     NOW() AS data_processamento
 FROM 's3://silver/raw/empresas.parquet'
+--Necessário pelo registro duplicado e sem razão social na base de 06/2026
+WHERE column0 != '08314885' AND column1 IS NOT NULL
