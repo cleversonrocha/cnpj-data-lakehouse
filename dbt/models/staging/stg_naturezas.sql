fd831d4d6579d38359318c09b10e599a806b5b0b
Cleverson Leite da Rocha <cleverson.rocha82@gmail.com>
2026-07-08 12:14:25 -0300

    feat: Formatando o código da natureza jurídica.

--- dbt/models/staging/stg_naturezas.sql
+++ dbt/models/staging/stg_naturezas.sql
@@ -4,8 +4,9 @@
     ]
 ) }}
 
-SELECT 
+SELECT
     CAST(column0 AS INTEGER) AS codigo,
+    SUBSTR(column0, 1, 3) || '-' ||  SUBSTR(column0, 4, 1) AS codigo_formatado,
     column1 AS descricao,    
     NOW() AS data_processamento
 FROM {{ get_s3_path(base_path='silver/raw', file_name='naturezas') }}
