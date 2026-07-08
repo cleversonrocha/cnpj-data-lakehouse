c2e3ea53ce69f52c8eb86e3a2b67d327aeee394a
Cleverson Leite da Rocha <cleverson.rocha82@gmail.com>
2026-07-08 12:14:49 -0300

    feat: Formatando o código do cnae.

--- dbt/models/marts/dim_cnaes.sql
+++ dbt/models/marts/dim_cnaes.sql
@@ -7,5 +7,13 @@
 SELECT
     CAST(ROW_NUMBER() OVER(order by codigo) AS INTEGER) AS sk_id,    
     codigo,
+    CASE 
+        WHEN LENGTH(codigo) = 7 THEN
+            SUBSTR(codigo, 1, 2) || '.' ||
+            SUBSTR(codigo, 3, 2) || '-' ||
+            SUBSTR(codigo, 5, 1) || '-' ||
+            SUBSTR(codigo, 6, 2)
+        ELSE codigo 
+    END AS codigo_formatado,
     descricao
 FROM {{ ref('stg_cnaes') }}
