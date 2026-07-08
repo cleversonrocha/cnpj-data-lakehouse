90f4cac34c719d8bd21ca4464aeca790c440a75a
Cleverson Leite da Rocha <cleverson.rocha82@gmail.com>
2026-07-08 16:24:18 -0300

    fix: Alterando ordem das colunas.

--- dbt/models/staging/stg_empresas.sql
+++ dbt/models/staging/stg_empresas.sql
@@ -9,9 +9,9 @@ SELECT
     CAST(column1 AS VARCHAR) AS razao_social,
     CAST(column2 AS INTEGER) AS natureza_juridica,
     CAST(column3 AS INTEGER) AS qualificacao_responsavel,
+    TRY_CAST(REPLACE(column4, ',', '.') AS DECIMAL(15, 2)) AS capital_social,
     CAST(column5 AS INTEGER) AS porte_empresa,
-    CAST(column6 AS VARCHAR) AS ente_federativo_responsavel,         
-    TRY_CAST(REPLACE(column4, ',', '.') AS DECIMAL(15, 2)) AS capital_social,        
+    CAST(column6 AS VARCHAR) AS ente_federativo_responsavel,    
     NOW() AS data_processamento
 FROM {{ get_s3_path(base_path='silver/raw', file_name='empresas') }}
 --Necessário pelo registro duplicado '08314885' e sem razão social na base de 06/2026
